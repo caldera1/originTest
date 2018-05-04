d1838d2032e492d4c5e6f6181171d5d1fc894b9b
Alejandro <alejandroe.calderon@gmail.com>
2018-05-04 11:48:47 -0300

    a

--- src/main/java/es/test/jenkins/DemoServletInitializer.java
+++ src/main/java/es/test/jenkins/DemoServletInitializer.java
@@ -2,11 +2,11 @@ package es.test.jenkins;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import es.test.jenkins.DemoApplication;
+
 
 public class DemoServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	return builder.sources(DemoApplication.class);
+	return builder.sources(es.test.jenkins.DemoApplication.class);
     }
 }
