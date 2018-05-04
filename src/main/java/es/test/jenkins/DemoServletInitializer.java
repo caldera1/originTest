d684c527f3956925828e38e011d6d4cdeaef8cb8
Alejandro <alejandroe.calderon@gmail.com>
2018-05-04 11:46:18 -0300

    mejora class

--- src/main/java/es/test/jenkins/DemoServletInitializer.java
+++ src/main/java/es/test/jenkins/DemoServletInitializer.java
@@ -2,6 +2,7 @@ package es.test.jenkins;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import es.test.jenkins.DemoApplication;
 
 public class DemoServletInitializer extends SpringBootServletInitializer {
     @Override
